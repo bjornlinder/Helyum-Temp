74b5a2ec1a2b44f0043f5481034b22d6b55cac7e
Bjorn Linder <bjornishungry@hotmail.com>
2014-05-12 17:00:53 -0400

    notice added for successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to root_path
+      flash[:notice] = "Thank you #{@user.first_name}! Username #{@user.username} has been reserved."
+    else
+      render :new
     end
   end
 
