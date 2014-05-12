cb1026a3f15a216ea9235d878b7b46da632aeef4
Bjorn Linder <bjornishungry@hotmail.com>
2014-05-12 16:09:47 -0400

    deleted unwanted files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+  validates :username, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140512193613) do
+ActiveRecord::Schema.define(version: 20140512194258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20140512193613) do
     t.string   "first_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username",   null: false
+    t.string   "last_name",  null: false
+    t.string   "email",      null: false
   end
 
 end
