5a0aad04d9d287427cd1ec7708df3fb9d827b16a
Bjorn Linder <bjornishungry@hotmail.com>
2014-08-07 13:25:38 -0400

    changed form type to get so comments are not lost on validation failure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    @user = User.new(user_params)
+    @email = Email.new
+
+    if @user.save
+      redirect_to root_path
+      flash[:notice] = "Thank you #{@user.first_name}! We will let you know when we launch."
+    # Username '#{@user.username}' has been reserved."
+    else
+      render :join
+    end
   end
 
   def new
@@ -13,21 +22,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
-  def create
-    @user = User.new(user_params)
-    @email = Email.new
-
-    if @user.save
-      redirect_to root_path
-      flash[:notice] = "Thank you #{@user.first_name}! Username '#{@user.username}' has been reserved."
-    else
-      render :join
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   @email = Email.new
+  #
+  #   if @user.save
+  #     redirect_to root_path
+  #     flash[:notice] = "Thank you #{@user.first_name}! Username '#{@user.username}' has been reserved."
+  #   else
+  #     render :join
+  #   end
+  # end
 
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :username)
+      params.require(:user).permit(:first_name, :last_name, :email, :username, :suggestions)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true, length: { maximum: 30 }
   validates :last_name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
   validates :username, presence: true, uniqueness: true, length: { maximum: 30 }
+  validates :suggestions, length: { maximum: 255 }
 end
